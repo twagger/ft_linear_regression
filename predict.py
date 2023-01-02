332778c493226d28d6c97708e355ca6465105e95
twagner <twagner@student.42.fr>
2023-01-02 16:13:40 +0100

    init: fist commit with empty files to init

---
+++ predict.py
@@ -0,0 +1,15 @@
+"""
+Module to use a model to predict the price of a car according to its mileage
+"""
+import numpy as np
+
+
+if __name__ == "__main__":
+
+    # check if a parameter file exists with proper thetas
+
+    # if not : thetas = 0
+
+    # prompt for a mileage
+
+    # print the price estimated by the model
---
+++ train.py
@@ -0,0 +1,17 @@
+"""Module to train a linear regression model"""
+import numpy from np
+
+
+if __name__ == "__main__":
+
+    # check if a previous file contains initial thetas
+
+    # if not, initial thetas = 0
+
+    # perform a predict / evaluate / improve
+
+    # store the final thetas
+
+    # Option : plot the data into a scatter grap
+
+    # Option : plot the prediction function into the same graph
